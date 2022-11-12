1a2a160e26900d806b35fbfcf7d24f6e9e62a85d
parkdin <abdukokhorovizatillo00@gmail.com>
2022-11-12 17:33:56 +0900

    done

--- file04.py
+++ file04.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    f = open(data).read()
+    num = []
+    non_digit = []
+    for i in f:
+        if i.isdigit():
+            num += i
+        else:
+            non_digit += i
+    return non_digit
+print(main('txt_file/data04.txt'))
 # Read data from file

146ed797dffa63d522eba692e7dbdff25c2c688d
parkdin <abdukokhorovizatillo00@gmail.com>
2022-11-13 19:55:43 +0900

    done

--- file08.py
+++ file08.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    max_char = 0
+    for i in data:
+        if i.isdigit():
+            if int(i) > max_char:
+                max_char = int(i)
+    return max_char
 # Read data from file
+data = open('txt_file/data08.txt').read()
+print(main(data))
